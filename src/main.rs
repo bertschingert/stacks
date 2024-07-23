a18561ee0bc5801471604c16476f50753c07dbb9
Thomas Bertschinger <tahbertschinger@gmail.com>
2024-07-22 19:10:24 -0600

    Add arguments to only include user threads or kernel threads

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,99 @@
-use std::{
-    fs,
-    path,
-    collections::HashMap,
-    rc::Rc,
-};
+use std::{collections::HashMap, fs, path, rc::Rc};
+
+struct Options {
+    include_kernel: bool,
+    include_user: bool,
+}
+
+fn usage() {
+    eprintln!("Usage: stacks [ku]");
+    eprintln!("         k: only include kernel threads");
+    eprintln!("         u: only include user threads");
+}
+
+impl Options {
+    fn default_options() -> Options {
+        Options {
+            include_kernel: true,
+            include_user: true,
+        }
+    }
+
+    fn apply(&mut self, opts: Vec<String>) {
+        for s in opts.iter().skip(1) {
+            for ch in s.chars() {
+                match ch {
+                    'k' => self.include_user = false,
+                    'u' => self.include_kernel = false,
+                    _ => {
+                        usage();
+                        std::process::exit(1);
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ProcStat {
+    ppid: usize,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseProcStatError;
+
+impl std::str::FromStr for ProcStat {
+    type Err = ParseProcStatError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // fields we care about start after final ')', which terminates comm name
+        let rest = s.split(')').last().ok_or(ParseProcStatError)?;
+
+        let mut fields = rest.split(' ');
+        let ppid: usize = fields
+            .nth(2)
+            .and_then(|s| s.parse().ok())
+            .ok_or(ParseProcStatError)?;
+
+        Ok(ProcStat { ppid })
+    }
+}
 
 struct ProcEntry {
-    _pid: usize,
+    pid: usize,
     comm: String,
     stack: String,
 }
 
-fn get_proc_ent(path: &path::Path) -> Option<ProcEntry> {
-    let _pid = match path.file_name().unwrap()
-        .to_str().unwrap()
-        .parse::<usize>() {
+fn read_proc_data(path: &path::Path, file: &str) -> Result<String, std::io::Error> {
+    // TODO: switch to inspect_err() once on a new enough rustc version
+    fs::read_to_string(path.join(file)).map_err(|e| {
+        eprintln!("Could not read {}: {}", path.join(file).display(), e);
+        e
+    })
+}
+
+fn get_proc_ent(options: &Options, path: &path::Path) -> Option<ProcEntry> {
+    let pid: usize = match path.file_name().unwrap().to_str().unwrap().parse() {
         Err(_) => return None,
         Ok(p) => p,
     };
 
-    let comm = match fs::read_to_string(path.join("comm")) {
-        Err(e) => {
-            eprintln!("could not read {}: {}", path.join("comm").display(), e);
+    let stat: ProcStat = read_proc_data(path, "stat").ok()?.parse().ok()?;
+
+    if stat.ppid == 2 || pid == 2 {
+        if !options.include_kernel {
             return None;
-        },
-        Ok(mut s) => {
-            s.pop(); /* trim newline from comm */
-            s
         }
-    };
+    } else if !options.include_user {
+        return None;
+    }
 
-    let stack = match fs::read_to_string(path.join("stack")) {
-        Err(e) => {
-            eprintln!("could not read {}: {}", path.join("stack").display(), e);
-            return None;
-        },
-        Ok(s) => s,
-    };
+    let comm = read_proc_data(path, "comm").ok()?;
 
-    Some(ProcEntry { _pid, comm, stack, })
+    let stack = read_proc_data(path, "stack").ok()?;
+
+    Some(ProcEntry { pid, comm, stack })
 }
 
 fn main() {
@@ -46,17 +101,21 @@ fn main() {
 
     let mut proc_hash: HashMap<String, Vec<Rc<ProcEntry>>> = HashMap::new();
 
+    let mut options = Options::default_options();
+    options.apply(std::env::args().collect());
+
     for entry in fs::read_dir(proc_path).unwrap() {
         let entry = entry.unwrap();
 
-        let proc_ent = match get_proc_ent(&entry.path()) {
+        let proc_ent = match get_proc_ent(&options, &entry.path()) {
             None => continue,
             Some(p) => p,
         };
 
         let proc_ent = Rc::new(proc_ent);
 
-        proc_hash.entry(proc_ent.stack.clone())
+        proc_hash
+            .entry(proc_ent.stack.clone())
             .and_modify(|v| v.push(Rc::clone(&proc_ent)))
             .or_insert(vec![Rc::clone(&proc_ent)]);
     }
